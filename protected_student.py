71ba6729559546398212156127834e055115e6f8
zbh123 <43636193+zbh123@users.noreply.github.com>
2018-10-15 18:53:37 +0800

    Create protected_student.py

---
+++ protected_student.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+class Student(object):
+
+    def __init__(self, name, score):
+        self.__name = name
+        self.__score = score
+
+    def get_name(self):
+        return self.__name
+
+    def get_score(self):
+        return self.__score
+
+    def set_score(self, score):
+        if 0 <= score <= 100:
+            self.__score = score
+        else:
+            raise ValueError('bad score')
+
+    def get_grade(self):
+        if self.__score >= 90:
+            return 'A'
+        elif self.__score >= 60:
+            return 'B'
+        else:
+            return 'C'
+
+bart = Student('Bart Simpson', 59)
+print('bart.get_name() =', bart.get_name())
+bart.set_score(60)
+print('bart.get_score() =', bart.get_score())
+
+print('DO NOT use bart._Student__name:', bart._Student__name)
+class Student(object):
+    def __init__(self, name, gender):
+        self.name = name
+        self.__gender = gender
+    def get_gender(self):
+        return self.__gender
+    def set_gender(self,gender):
+        self.__gender = gender
+bart = Student('Bart', 'male')
+if bart.get_gender() != 'male':
+    print('测试失败!')
+else:
+    bart.set_gender('female')
+    if bart.get_gender() != 'female':
+        print('测试失败!')
+    else:
+        print('测试成功!')
+        
