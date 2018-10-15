3016a56a04cc1027adfc58f4f1b25b10586096f6
zbh123 <43636193+zbh123@users.noreply.github.com>
2018-10-15 14:08:56 +0800

    Create do_reduce.py

---
+++ do_reduce.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from functools import reduce
+
+CHAR_TO_INT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9
+}
+
+def str2int(s):
+    ints = map(lambda ch: CHAR_TO_INT[ch], s)
+    return reduce(lambda x, y: x * 10 + y, ints)
+
+print(str2int('0'))
+print(str2int('12300'))
+print(str2int('0012345'))
+
+CHAR_TO_FLOAT = {
+    '0': 0,
+    '1': 1,
+    '2': 2,
+    '3': 3,
+    '4': 4,
+    '5': 5,
+    '6': 6,
+    '7': 7,
+    '8': 8,
+    '9': 9,
+    '.': -1
+}
+
+def str2float(s):
+    nums = map(lambda ch: CHAR_TO_FLOAT[ch], s)
+    point = 0
+    def to_float(f, n):
+        nonlocal point
+        if n == -1:
+            point = 1
+            return f
+        if point == 0:
+            return f * 10 + n
+        else:
+            point = point * 10
+            return f + n / point
+    return reduce(to_float, nums, 0.0)
+
+print(str2float('0'))
+print(str2float('123.456'))
+print(str2float('123.45600'))
+print(str2float('0.1234'))
+print(str2float('.1234'))
+print(str2float('120.0034'))
+
+def prod(L):
+  return reduce(lambda x,y:x*y,L)
+print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+if prod([3, 5, 7, 9]) == 945:
+    print('测试成功!')
+else:
+    print('测试失败!')
+    
+from functools import reduce
+def str2float(s):
+    def fn(x,y):
+        return x*10+y
+    n=s.index('.')
+    s1=list(map(int,[x for x in s[:n]]))
+    s2=list(map(int,[x for x in s[n+1:]]))
+    return reduce(fn,s1) + reduce(fn,s2)/10**len(s2)
+print('\'123.4567\'=',str2float('123.4567'))   
