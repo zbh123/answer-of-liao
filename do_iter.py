9d334e022adffc0c18c684401ac3ad782b3ab5a5
zbh123 <43636193+zbh123@users.noreply.github.com>
2018-10-15 10:27:26 +0800

    Create do_iter.py

---
+++ do_iter.py
@@ -0,0 +1,119 @@
+Skip to content
+ 
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ @zbh123 Sign out
+379
+2,789 2,930 michaelliao/learn-python3
+ Code  Issues 4  Pull requests 7  Projects 0  Wiki  Insights
+learn-python3/samples/advance/do_iter.py
+aed8fae  on 16 May 2015
+@michaelliao michaelliao update iter sample
+     
+Executable File  65 lines (51 sloc)  1.47 KB
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from collections import Iterable, Iterator
+
+def g():
+    yield 1
+    yield 2
+    yield 3
+
+print('Iterable? [1, 2, 3]:', isinstance([1, 2, 3], Iterable))
+print('Iterable? \'abc\':', isinstance('abc', Iterable))
+print('Iterable? 123:', isinstance(123, Iterable))
+print('Iterable? g():', isinstance(g(), Iterable))
+
+print('Iterator? [1, 2, 3]:', isinstance([1, 2, 3], Iterator))
+print('Iterator? iter([1, 2, 3]):', isinstance(iter([1, 2, 3]), Iterator))
+print('Iterator? \'abc\':', isinstance('abc', Iterator))
+print('Iterator? 123:', isinstance(123, Iterator))
+print('Iterator? g():', isinstance(g(), Iterator))
+
+# iter list:
+print('for x in [1, 2, 3, 4, 5]:')
+for x in [1, 2, 3, 4, 5]:
+    print(x)
+
+print('for x in iter([1, 2, 3, 4, 5]):')
+for x in iter([1, 2, 3, 4, 5]):
+    print(x)
+
+print('next():')
+it = iter([1, 2, 3, 4, 5])
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+d = {'a': 1, 'b': 2, 'c': 3}
+
+# iter each key:
+print('iter key:', d)
+for k in d.keys():
+    print('key:', k)
+
+# iter each value:
+print('iter value:', d)
+for v in d.values():
+    print('value:', v)
+
+# iter both key and value:
+print('iter item:', d)
+for k, v in d.items():
+    print('item:', k, v)
+
+# iter list with index:
+print('iter enumerate([\'A\', \'B\', \'C\']')
+for i, value in enumerate(['A', 'B', 'C']):
+    print(i, value)
+
+# iter complex list:
+print('iter [(1, 1), (2, 4), (3, 9)]:')
+for x, y in [(1, 1), (2, 4), (3, 9)]:
+    print(x, y)
+© 2018 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Help
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
+Press h to open a hovercard with more details.
+
+def findMinAndMax(L):
+  if len(L) == 0:
+    return(None,None)
+  elif len(L) == 1:
+    return (L[0],L[0])
+  else:
+    min = L[0]
+    max = L[0]
+    for l in L :
+      if l > max:
+        max = l
+      if l < min :
+        min = l
+    return (min,max)
+if findMinAndMax([]) != (None, None):
+    print('测试失败!')
+elif findMinAndMax([7]) != (7, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1]) != (1, 7):
+    print('测试失败!')
+elif findMinAndMax([7, 1, 3, 9, 5]) != (1, 9):
+    print('测试失败!')
+else:
+    print('测试成功!')
